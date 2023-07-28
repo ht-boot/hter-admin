4f72f58625d16278d706181a6c3c6c45a62e0361
ht <1165006789@qq.com>
2023-07-28 19:50:43 +0800

    layout

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,7 +1,16 @@
 <template>
-  <LayoutVertical />
+  <el-config-provider :locale="locale">
+    <LayoutVertical />
+  </el-config-provider>
 </template>
 
 <script lang="ts" setup>
+import { computed, ref } from "vue";
 import LayoutVertical from "@/layout/layoutVertical/Layout.vue";
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
+import en from "element-plus/dist/locale/en.mjs";
+
+const language = ref("zh-cn");
+
+const locale = computed(() => (language.value === "zh-cn" ? zhCn : en));
 </script>
